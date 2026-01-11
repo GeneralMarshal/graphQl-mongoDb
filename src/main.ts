77fd0d3b530d3666ab4b7b13169b56ab3289df1c
GeneralMarshal <112048946+GeneralMarshal@users.noreply.github.com>
2026-01-11 07:00:20 +0100

    Lessons entity

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Explicitly set up JSON body parser
+  app.use(json({ limit: '10mb' }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
